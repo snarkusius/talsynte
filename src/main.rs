e3bb569baeb13ee8387e019c1ae66a92dc87a575
snarkusius <marcus@lengquist.se>
2021-05-23 22:10:12 +0200

    talsyntes working

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ mod wavConcatenation;
 mod strToFonem;
 mod wavMultConcatenation;
 fn main() {
-    let mut base_str = String::from("barock andas av bet");
+    let mut base_str = String::from("a b c b d e f g h i j k l m n o p");
     let mut return_str = String::from("");
-    let mut numbers: Vec<&str> = vec![];
+    let mut fonemfiles: Vec<String> = vec![];
     // println!("{}",strToFonem::run("andas"));
     for word in base_str.split_whitespace() {
         return_str.push_str(strToFonem::run(word));
@@ -15,11 +15,17 @@ fn main() {
     for s in return_str.split('+') {
         println!("{}", s)
     }
-    for s in return_str.split('+') {
-        println!("{}", s)
+    for word in return_str.split_whitespace() {
+        for fonem in word.split('+') {
+            let mut filenam = String::from("fonem/");
+            filenam.push_str(fonem);
+            filenam.push_str(".wav");
+            fonemfiles.push(filenam);
+        }
+        fonemfiles.push(String::from("fonem/silence500ms.wav"));
     }
-    // return_str.split_whitespace()
+    
     // concatenate_wavs("sine.wav", "sine1.wav", "test2.wav" );
-    wavMultConcatenation::run(vec!["fonem/aaa.wav", "fonem/p.wav","fonem/a_.wav"], "apa.wav");
+    wavMultConcatenation::run(fonemfiles, "testis.wav");
 
 }
--- src/wavMultConcatenation.rs
+++ src/wavMultConcatenation.rs
@@ -1,10 +1,11 @@
 extern crate hound;
 use hound::{SampleFormat, WavSpec, WavWriter};
-pub fn run(filesInput: Vec<&str>, filename: &str){
-    let header =hound::WavReader::open(filesInput[0]).unwrap().spec();
+pub fn run(filesInput: Vec<String>, filename: &str){
+    println!("{:?}",filesInput[0].as_str());
+    let header =hound::WavReader::open(filesInput[0].as_str()).unwrap().spec();
     let mut writer = WavWriter::create(filename, header).expect("Failed to created WAV writer for output file");
     for file in filesInput.iter() {
-        let mut reader = hound::WavReader::open(file).unwrap();
+        let mut reader = hound::WavReader::open(file.as_str()).unwrap();
         let signal = reader.samples::<i16>();
         for x in signal {
             writer.write_sample(x.unwrap()).unwrap();
