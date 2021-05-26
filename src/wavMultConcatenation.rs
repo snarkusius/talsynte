fa181d04a3d1a0361463e350cbea3a3a1a4c4100
snarkusius <marcus@lengquist.se>
2021-05-26 13:56:22 +0200

    som clenup

--- src/wavMultConcatenation.rs
+++ src/wavMultConcatenation.rs
@@ -12,14 +12,3 @@ pub fn run(filesInput: Vec<String>, filename: &str){
         }
     }
 }
-    // let mut reader1 = hound::WavReader::open(filesInput[0]).unwrap();
-    // let mut reader2 = hound::WavReader::open(filesInput[1]).unwrap();
-
-    // // gör om reders till pekare på vektorer/wav filen
-    // let signal1 = reader1.samples::<i16>();
-    // let signal2 = reader2.samples::<i16>();
-    
-
-    // for x in signal1.chain(signal2) {
-    //     writer.write_sample(x.unwrap()).unwrap();
-    // }
